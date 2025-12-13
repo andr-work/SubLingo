20a6bab5357db245fcd03aa728413d1543c061fa
andr-work <andr21work@gmail.com>
2025-12-14 00:34:31 +0900

    add Dockerfile

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
